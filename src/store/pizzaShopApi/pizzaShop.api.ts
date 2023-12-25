87165750c1617128e96f8daa143c1235678d3a69
Vlad Sh <kausvirtual@gmail.com>
2023-12-25 02:01:03 +0200

    auto refresh refreshTokens by get 401 or logout by failure

--- src/store/pizzaShopApi/pizzaShop.api.ts
+++ src/store/pizzaShopApi/pizzaShop.api.ts
@@ -1,20 +1,53 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  FetchArgs,
+  createApi,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { logout, setCredentials } from "../auth/auth.slice";
 const baseUrl = import.meta.env.VITE_BASE_URL;
+
+const baseQuery = fetchBaseQuery({
+  baseUrl,
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set("Authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithResult = async (
+  args: string | FetchArgs,
+  api: BaseQueryApi,
+  extraOptions: Record<string, unknown> //! change this in the future, for type safety
+) => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status === 401) {
+    console.log("sending refresh token");
+    const refreshResult = await baseQuery(
+      "/auth/refreshTokens",
+      api,
+      extraOptions
+    );
+    console.log(refreshResult);
+    if (refreshResult?.data) {
+      const user = (api.getState() as RootState).auth.user;
+      api.dispatch(setCredentials({ ...refreshResult.data, user }));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+  return result;
+};
 
 export const pizzaShopApi = createApi({
   reducerPath: "pizzaShopApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl,
-    credentials: "include",
-    prepareHeaders: (headers, { getState }) => {
-      const token = (getState() as RootState).auth.token;
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithResult,
   tagTypes: ["Test"],
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   endpoints: (_) => ({}),
