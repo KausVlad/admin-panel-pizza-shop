21596ff33ba5dc004d48316023f4820cbbc30b2d
Vlad Sh <kausvirtual@gmail.com>
2024-03-05 22:16:14 +0200

    Revert "added updateIngredient endpoint"
    This reverts commit bc9b58fa83658101505a6c0d81f6506cc78b1e9d.

--- src/utils/toggleDialog.ts
+++
@@ -1,8 +0,0 @@
-export const toggleDialog = (dialogRef: React.RefObject<HTMLDialogElement>) => {
-  if (!dialogRef.current) {
-    return;
-  }
-  dialogRef.current.hasAttribute("open")
-    ? dialogRef.current.close()
-    : dialogRef.current.showModal();
-};
